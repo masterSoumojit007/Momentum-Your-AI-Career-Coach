0864453d5df07a9c7c0861c6014b6f9de2f0cf94
Soumojit Banerjee <soumojitbanerjee22@gmail.com>
2025-01-31 20:00:45 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com", // Add this line
+      },
+    ],
+    domains: ['images.unsplash.com'],  // Add Unsplash domain here
+  },
+};
 
 export default nextConfig;
